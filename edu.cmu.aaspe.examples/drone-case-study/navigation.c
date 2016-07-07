3d670b8fc127b32231ef80d1951e7c47342cc2af
Julien Delange <julien@gunnm.org>
2016-07-07 16:28:30 -0400

    update navigation code

--- edu.cmu.aaspe.examples/drone-case-study/navigation.c
+++ edu.cmu.aaspe.examples/drone-case-study/navigation.c
@@ -2,7 +2,25 @@
 #include <generatedtypes.h>
 
 
-void navigation (navdatat navdata)
+//typedef enum {
+//	navdatat_up = 0
+//	,navdatat_down = 1
+//	,navdatat_left = 2
+//	,navdatat_right = 3
+//}navdatat;
+
+int iteration = 0;
+
+void navigation (navdatat* navdata)
 {
 	print_navdata ("NAVIGATION", navdata);
+	if (iteration < 10)
+	{
+		*navdata = navdatat_up;
+	}
+	if ( (iteration >= 10) && (iteration < 20))
+	{
+		*navdata = navdatat_up;
+	}
+	iteration = (iteration + 1) % 20;
 }
